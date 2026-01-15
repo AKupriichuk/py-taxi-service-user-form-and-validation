d60980bff0e53835ca3b69932e2f83510504571a
AKupriichuk <alexa05kup@gmail.com>
2026-01-15 23:01:29 +0200

    c1

---
+++ taxi/forms.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from django import forms
+from taxi.models import Driver
+from django.contrib.auth import get_user_model
+from .models import Car
+
+
+def validate_license_number(license_number):
+    if (len(license_number) != 8
+            or not license_number[:3].isalpha()
+            or not license_number[:3].isupper()
+            or not license_number[3:].isdigit()):
+        raise ValidationError(
+            "License must be 8 characters: 3 uppercase letters and 5 digits"
+        )
+    return license_number
+
+
+class DriverLicenseUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Driver
+        fields = ("license_number",)
+    def clean_license_number(self):
+        return validate_license_number(self.cleaned_data["license_number"])
+
+
+class DriverCreateForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Driver
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "license_number",
+    )
+
+    def clean_license_number(self):
+        return validate_license_number(self.cleaned_data["license_number"])
+
+
+class CarForm(forms.ModelForm):
+  drivers = forms.ModelMultipleChoiceField(
+    queryset=get_user_model().objects.all(),
+    widget=forms.CheckboxSelectMultiple,
+    required=False
+  )
+
+  class Meta:
+    model = Car
+    fields = "__all__"
